49f610355c71b03f6a045ae50ad69d2a04da2dc3
arijhajji <61475226+arijhajji-1@users.noreply.github.com>
2023-02-06 20:08:46 +0100

    first commit

---
+++ AM.ApplicationCore/Services/ServiceFlight.cs
@@ -0,0 +1,112 @@
+﻿using AM.ApplicationCore.Infterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Services
+{
+    public class ServiceFlight : IServiceFlight
+    {
+        public List<Flight> Flights { get; set;} = new List<Flight>();
+
+        /*List<DateTime> GetFlightDates(string destination)
+        {
+            List<DateTime> result = new List<DateTime>();
+            for (int i = 0; i < Flights.Count; i++)
+            {
+                Flight flight = Flights[i];
+                if (flight.Destination == destination)
+                {
+                    result.Add(flight.FlightDate);
+                }
+            }
+            return result;
+        }*/
+
+        /*IEnumerable<DateTime> GetFlightDates(string destination)
+        {
+            for (int i = 0; i < Flights.Count; i++)
+            {
+                Flight flight = Flights[i];
+                if (flight.Destination == destination)
+                {
+                    yield return flight.FlightDate;
+                }
+            }
+        }*/
+
+        IEnumerable<DateTime> GetFlightDates(string destination)
+        {
+            foreach (var flight in Flights)
+            {
+                if (flight.Destination == destination)
+                {
+                    yield return flight.FlightDate;
+                }
+            }
+        }
+
+        public void GetFlights(string filterType, string filterValue)
+        {
+            switch (filterType)
+            {
+                case "Destination":
+                    {
+                        var result = Flights.Where(f => f.Destination == filterValue).ToList();
+                        foreach (var f in result)
+                        {
+                            Console.WriteLine(f);
+                        }
+                    }
+                    break;
+                case "Departure":
+                    {
+                        var result = Flights.Where(f => f.Departure == filterValue).ToList();
+                        foreach (var f in result)
+                        {
+                            Console.WriteLine(f);
+                        }
+                    }
+                    break;
+                case "FlightDate":
+                    {
+                        var result = Flights.Where(f => f.FlightDate == DateTime.Parse(filterValue)).ToList();
+                        foreach (var f in result)
+                        {
+                            Console.WriteLine(f);
+                        }
+                    }
+                    break;
+                case "FlightId":
+                    {
+                        var result = Flights.Where(f => f.FlightId == int.Parse(filterValue)).ToList();
+                        foreach (var f in result)
+                        {
+                            Console.WriteLine(f);
+                        }
+                    }
+                    break;
+                case "EffectiveArrival":
+                    {
+                        var result = Flights.Where(f => f.EffectiveArrival == DateTime.Parse(filterValue)).ToList();
+                        foreach (var f in result)
+                        {
+                            Console.WriteLine(f);
+                        }
+                    }
+                    break;
+                case "EstimatedDuration":
+                    {
+                        var result = Flights.Where(f => f.EstimatedDuration == int.Parse(filterValue)).ToList();
+                        foreach (var f in result)
+                        {
+                            Console.WriteLine(f);
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ AM.UI.Console/Program.cs
@@ -0,0 +1,91 @@
+﻿using AM.ApplicationCore;
+using AM.ApplicationCore.Domaine;
+using AM.ApplicationCore.Services;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AM.UI.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Plane plane = new Plane();
+            plane.MyPlaneType = PlaneType.Airbus;
+            plane.Capacity = 200;
+            plane.ManufactureDate = new DateTime(2018, 11, 10);
+
+
+            Plane p2 = new Plane(PlaneType.Boing, 300, DateTime.Now);
+            System.Console.WriteLine(p2);
+            Plane p = new Plane
+            {
+                MyPlaneType = PlaneType.Airbus,
+                Capacity = 150,
+                ManufactureDate = new DateTime(2015, 02, 03)
+            };
+            Boolean x;
+
+            System.Console.WriteLine(p);
+            Passenger passenger = new Passenger();
+            x = passenger.CheckProfile("arij", "hajji", "arij.hajji@esprit.tn");
+            System.Console.WriteLine(x);
+            passenger.GetPassengerType();
+
+            Staff staff = new Staff();
+            staff.GetPassengerType();
+            Traveller traveller = new Traveller();
+            traveller.GetPassengerType();
+
+
+            System.Console.WriteLine(passenger.GetPassengerType());
+            System.Console.WriteLine(staff.GetPassengerType());
+            System.Console.WriteLine(traveller.GetPassengerType());
+
+            /*ArrayList list= new ArrayList();
+            list.Add(10);
+            list.Add("ABC");
+            list.Add('t');
+
+            for (int i = 0; i < list.Count ; i++)
+            {
+                System.Console.WriteLine(list[i]);
+            }
+
+
+            foreach (var item in list)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            List<Passenger> list2= new List<Passenger>();
+            list2.Add(new Passenger() { BirthDate = DateTime.Now, FirstName="Foulen",LastName="Fouleni" });
+
+            List<Passenger> list3= new List<Passenger>()
+            {
+                new () {BirthDate = DateTime.Now, FirstName="Foulen",LastName="Fouleni"},
+                new Passenger() {BirthDate = DateTime.Now, FirstName="Foulen2",LastName="Fouleni2"}
+            };
+
+            List<Traveller> list4= new List<Traveller>()
+            {
+                new () {BirthDate = DateTime.Now, Nationality="TN"}
+            };
+            List<Staff> list5= new List<Staff>()
+            {
+                new () {BirthDate = DateTime.Now, PasseportNumber="78946413TN"}
+            };
+
+            //list3.AddRange(list4);
+
+            list3 = new List<Passenger>(list4);*/
+
+            ServiceFlight serviceFlight = new ServiceFlight();
+
+            serviceFlight.Flights = TestData.Flights;
+
+
+
+        }
+    }
+}
